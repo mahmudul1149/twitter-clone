73fd3686d356a3d8204ee4ce675c5a62e4f5ad8d
mahmudul <imdmahmudul237@gmail.com>
2022-09-25 18:34:41 +0600

    Completed Header Section

---
+++ pages/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container-box main-app-container">
+    <div class="left-sidebar">
+      <header-vue></header-vue>
+    </div>
+    <div class="main-section"></div>
+    <div class="right-sidebar"></div>
+  </div>
+</template>
+
+<script>
+import HeaderVue from "../components/Header.vue";
+export default {
+  name: "IndexPage",
+  components: {
+    HeaderVue,
+  },
+};
+</script>
+<style lang="scss">
+.main-app-container {
+  width: 1200px;
+  margin: auto;
+}
+.container-box {
+  display: grid;
+  grid-template-columns: 250px auto 400px;
+  .box-2 {
+    border: 1px solid #ccc;
+  }
+}
+</style>
